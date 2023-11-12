478d4b4966904d4bda3809c5f6bce3bef80dcc4c
margotdv <margotdv@orange.fr>
2023-11-12 16:51:27 +0100

    fix

--- scripts/translations.js
+++ scripts/translations.js
@@ -1,17 +1,18 @@
 (function() {
-
   "use strict";
-  document.addEventListener("DOMContentLoaded",init);
+
+  document.addEventListener("DOMContentLoaded", init);
 
   let currentLanguage = localStorage.getItem('selectedLanguage') || 'fr';
 
   function init() {
-    console.log(currentLanguage);
     loadTranslation(currentLanguage);
-    
+
     let cvLink = document.querySelector("#cv-link");
-    (currentLanguage === 'en') ? cvLink.href = './resources/cv-margot-de-villiers-english.pdf' : cvLink.href = './resources/cv-margot-de-villiers.pdf';
-    
+    if (cvLink) {
+      (currentLanguage === 'en') ? cvLink.href = './resources/cv-margot-de-villiers-english.pdf' : cvLink.href = './resources/cv-margot-de-villiers.pdf';
+    }
+
     let languageBtns = document.querySelectorAll('.language-btn');
 
     // Ecouteur de clic sur les boutons
@@ -25,28 +26,29 @@
           localStorage.setItem('selectedLanguage', selectedLanguage);
         }
 
-        (selectedLanguage === 'en') ? cvLink.href = './resources/cv-margot-de-villiers-english.pdf' : cvLink.href = './resources/cv-margot-de-villiers.pdf';
+        let cvLink = document.querySelector("#cv-link");
+        if (cvLink) {
+          (selectedLanguage === 'en') ? cvLink.href = './resources/cv-margot-de-villiers-english.pdf' : cvLink.href = './resources/cv-margot-de-villiers.pdf';
+        }
       });
-    })
+    });
   }
 
   // Charger la traduction
   function loadTranslation(language) {
     fetch(`../locales/${language}.json`)
-    .then(response => response.json())
-    .then(translations => {
-      const elementsToUpdate = document.querySelectorAll('[data-i18n]');
+      .then(response => response.json())
+      .then(translations => {
+        const elementsToUpdate = document.querySelectorAll('[data-i18n]');
 
-      elementsToUpdate.forEach(element => {
-        const translationKey = element.getAttribute('data-i18n');
+        elementsToUpdate.forEach(element => {
+          const translationKey = element.getAttribute('data-i18n');
 
-        if (translations[translationKey]) {
-          element.innerHTML = translations[translationKey];
-        }
-      });
-    })
-
-    .catch(error => console.error('Error loading translation:', error));
+          if (translations[translationKey]) {
+            element.innerHTML = translations[translationKey];
+          }
+        });
+      })
+      .catch(error => console.error('Error loading translation:', error));
   }
-
 }());
